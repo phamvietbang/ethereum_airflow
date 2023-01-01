c4f7e6abdef09d64971c505f67f1091418e20f76
phamvietbang <superpower99x99@gmail.com>
2023-01-01 19:02:51 +0700

    update docker

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # airflow
+docker build . --tag extending_airflow:latest
+docker-compose up airflow-init
--- dags/export_data.py
+++ dags/export_data.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.decorators import task
 from airflow.operators.python import PythonOperator
 from tasks.export_block_transactions import export_blocks_transactions
 from tasks.export_receipt_logs import export_receipt_logs
@@ -17,7 +16,7 @@ item_exporter = S3StreamingExporter(
 )
 
 params = {
-    'start_block': None,
+    'start_block': 1580068,
     'end_block': None,
     'provider_uri': "https://rpc.onuschain.io/",
     'output': item_exporter,
@@ -44,78 +43,73 @@ def calculate_target_block(current_block, block_batch_size, end_block, last_sync
     target_block = min(target_block, end_block) if end_block is not None else target_block
     return target_block
 
+def get_start_end_block(start_block, end_block, block_batch_size, last_synced_block_file, provider_uri, ti):
+    if start_block is not None or not os.path.isfile(last_synced_block_file):
+        init_last_synced_block_file((start_block or 0) - 1, last_synced_block_file)
+
+    last_synced_block = read_last_synced_block(last_synced_block_file)
+    blocks_to_sync = 0
+    target_block = end_block
+    while (end_block is None or last_synced_block < end_block) and blocks_to_sync:
+        current_block = get_current_block_number(provider_uri)
+        target_block = calculate_target_block(current_block, block_batch_size, end_block, last_synced_block)
+        blocks_to_sync = max(target_block - last_synced_block, 0)
+
+        logging.info('Current block {}, target block {}, last synced block {}, blocks to sync {}'.format(
+            current_block, target_block, last_synced_block, blocks_to_sync))
+
+    ti.xcom_push(key='start_batch_block', value=last_synced_block + 1)
+    ti.xcom_push(key='end_batch_block', value=target_block)
+    # return last_synced_block + 1, target_block
+
+
+
+def get_transactions(ti, provider_uri, output, chain):
+    start_block = ti.xcom_pull(task_ids='Get_start_end_block', key='start_batch_block')
+    end_block = ti.xcom_pull(task_ids='Get_start_end_block', key='end_batch_block')
+    transactions = export_blocks_transactions(
+        start_block,
+        end_block,
+        provider_uri,
+        output,
+        chain,
+    )
+    hashes = []
+    for transaction in transactions:
+        hashes.append(transaction['hash'])
+
+    ti.xcom_push(key='hash', value=hashes)
+    # return hashes
+
+
+def get_logs(ti, provider_uri, output, chain):
+    transaction_hashes = ti.xcom_pull(task_ids='Get_transactions', key='hash')
+    logs = export_receipt_logs(
+        provider_uri=provider_uri,
+        output=output,
+        chain=chain,
+        transaction_hashes=transaction_hashes,
+    )
+    ti.xcom_push(key='logs', value=logs)
+    # return logs
+
+
+def get_events(ti, output, chain):
+    logs = ti.xcom_pull(task_ids='Get_logs', key='logs')
+    export_events(output=output, logs=logs, chain=chain)
+
+
+def write_last_updated_block(ti, file):
+    block = ti.xcom_pull(task_ids='Get_start_end_block', key='end_batch_block')
+    write_last_synced_block(file, block)
 
 with DAG(
         default_args=default_args,
         dag_id='Athena_03',
-        start_date=datetime(2022, 12, 26),
-        schedule_interval='*/1 * * * *',
-        max_active_runs=1
+        start_date=datetime(2023, 1, 1),
+        # schedule_interval='*/1 * * * *',
+        # max_active_runs=1
 ) as dag:
-    @task
-    def get_start_end_block(start_block, end_block, block_batch_size, last_synced_block_file, provider_id, ti):
-        if start_block is not None or not os.path.isfile(last_synced_block_file):
-            init_last_synced_block_file((start_block or 0) - 1, last_synced_block_file)
-
-        last_synced_block = read_last_synced_block(last_synced_block_file)
-        blocks_to_sync = 0
-        target_block = end_block
-        while (end_block is None or last_synced_block < end_block) and blocks_to_sync:
-            current_block = get_current_block_number(provider_id)
-            target_block = calculate_target_block(current_block, block_batch_size, end_block, last_synced_block)
-            blocks_to_sync = max(target_block - last_synced_block, 0)
-
-            logging.info('Current block {}, target block {}, last synced block {}, blocks to sync {}'.format(
-                current_block, target_block, last_synced_block, blocks_to_sync))
-        ti.xcom_push(key='start_batch_block', value=last_synced_block + 1)
-        ti.xcom_push(key='end_batch_block', value=target_block)
-        return last_synced_block + 1, target_block
-
-
-    @task
-    def get_transactions(ti, provider_uri, output, chain):
-        start_block = ti.xcom_pull(task_ids='Get_start_end_block', key='start_batch_block')
-        end_block = ti.xcom_pull(task_ids='Get_start_end_block', key='end_batch_block')
-        transactions = export_blocks_transactions(
-            start_block,
-            end_block,
-            provider_uri,
-            output,
-            chain,
-        )
-        hashes = []
-        for transaction in transactions:
-            hashes.append(transaction['hash'])
-
-        ti.xcom_push(key='hash', value=hashes)
-        return hashes
-
-
-    @task
-    def get_logs(ti, provider_uri, output, chain):
-        transaction_hashes = ti.xcom_pull(task_ids='Get_transactions', key='hash')
-        logs = export_receipt_logs(
-            provider_uri=provider_uri,
-            output=output,
-            chain=chain,
-            transaction_hashes=transaction_hashes,
-        )
-        ti.xcom_push(key='logs', value=logs)
-        return logs
-
-
-    @task
-    def get_events(ti, output, chain):
-        logs = ti.xcom_pull(task_ids='Get_logs', key='logs')
-        export_events(output=output, logs=logs, chain=chain)
-
-
-    @task
-    def write_last_updated_block(ti, file):
-        block = ti.xcom_pull(task_ids='Get_start_end_block', key='end_batch_block')
-        write_last_synced_block(file, block)
-
-
     task1 = PythonOperator(
         task_id='Get_start_end_block',
         python_callable=get_start_end_block,
